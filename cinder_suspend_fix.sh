3c8c45c402ccc0802d3c5d164a205b4ff7f3da04
seancarlisle <carlisle.sean@gmail.com>
2017-08-02 17:15:03 -0500

    This will be going away soon

--- cinder_suspend_fix.sh
+++ cinder_suspend_fix.sh
@@ -12,16 +12,23 @@
 
 volume_list=''
 volume_dir="/dev/cinder-volumes/"
-for i in $(dmsetup info "$(echo $volume_dir)"* | egrep -B1 'SUSPEND' | awk '/Name/ {print substr($2,17)}' | tr -s -);do
-   lvchange -ay $(echo $volume_dir)$i
-   mylist="$mylist $i"
+
+# Generate the list of suspended volumes and store 
+for i in $(/sbin/dmsetup info "$(echo $volume_dir)"* | egrep -B1 'SUSPEND' | awk '/Name/ {print substr($2,17)}' | tr -s -);do
+   
+   # Check if the volume is already in the file. If it is, attempt to resume it, else add it to the file
+   if [ $(grep $i /tmp/suspendedvols) -eq 0 ]; then         
+#     /sbin/lvchange -ay $(echo $volume_dir)$i
+      sed -i "/$i/ d" /tmp/suspendedvols
+      mylist="$mylist $i"
+   else
+      echo "$i" >> /tmp/suspendedvols
+   fi
 done
 
 if [ "$mylist" != "" ];then
-    output="The following volumes have been re-activated on host $(hostname):\n$mylist"
+    output="The following volumes have been re-activated on host $(hostname):\n$mylist" | mail -s "Suspended Volumes Resumed"
     echo -e $output | sed 's/ volume/\nvolume/g'
 fi
-
-echo "did we execute?" >> /tmp/myfile
 
 exit 0
